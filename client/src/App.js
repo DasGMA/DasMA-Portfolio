2eb8c2f33beb989784adddb7ef944fba7b502978
dasgma <dasgrigoma@gmail.com>
2019-02-12 16:34:35 -0800

    Navigation done and relevant pages are linked

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Navigation from './Components/Navigation/Navigation';
+import HomePage from './Components/HomePage/HomePage';
+import About from './Components/About/About';
+import Resume from './Components/Resume/Resume';
+import Projects from './Components/Projects/Projects';
+import Blog from './Components/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       Fresh install
+      <div>
+        <Navigation />
+          <Route exact path = '/' component = { HomePage } />
+          <Route path = '/about' component = { About } />
+          <Route path = '/resume' component = { Resume } />
+          <Route path = '/projects' component = { Projects } />
+          <Route path = '/blog' component = { Blog } />
       </div>
     );
   }
