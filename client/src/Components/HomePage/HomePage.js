961664d66a9b7454d83cfc6dd519af06e8ac3de4
dasgma <dasgrigoma@gmail.com>
2019-02-14 10:42:03 -0800

    Finished home page

--- client/src/Components/HomePage/HomePage.js
+++ client/src/Components/HomePage/HomePage.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-materialize';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
     return (
-        <main>
         <Container className = 'home-page'>
-        <img src={require ('../../Media/Dell.png')} className="bg" alt = 'Home page' />
+        <img src={require ('../../Media/Dell.png')} className = 'bg' alt = 'Home page' />
             <Row className = 'blue-grey-text text-lighten-5'>
-                <Col>
-                    <h1>Hi. My name is Das.</h1>
-                    <h2>I'm a software developer from California.</h2>
+                <Col s={12}>
+                    <h1>Hi. I'm Das.</h1>
+                    <h2>A software developer from California.</h2>
                     <h2>Welcome to my domain.</h2>
                 </Col>
             </Row>
+            <Row className = 'blue-grey-text text-lighten-5'>
+                <Col s={12} className = 'column'>
+                    <Link to = '/about'><i className="far fa-play-circle fa-5x meetme-icon"></i></Link>
+                </Col>
+            </Row>
         </Container>
-        </main>
     )
 }
 
