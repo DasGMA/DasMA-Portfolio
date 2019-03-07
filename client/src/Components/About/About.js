6df7097456a88f70d5d63e208060f07832b87a78
dasgma <dasgrigoma@gmail.com>
2019-03-06 17:49:54 -0800

    Projects are displaying. Need to fill up with info.

--- client/src/Components/About/About.js
+++ client/src/Components/About/About.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-materialize';
+import { Link } from 'react-router-dom';
 
 function About() {
     return (
         <Container style = {{paddingTop: '1rem'}}>
          <img className = 'bg' style = {{background: '#0B0B0B'}} alt = 'About me' />
             <Row>
-                <Col style = {{fontSize: '1.6rem', color: '#fff'}}>
-                    As a software developer, my prior knowledge in project management makes me an asset for any product-minded team.
-                    I've spent the past 9 months learning web development and computer science at Lambda School, an innovative software engineering academy.
-                    I hope to use my education and experience to provide a unique perspective when designing software solutions for customers.
-                    I love to learn new frameworks and languages in order to keep my skill-set sharp and to ensure the applications I'm designing are the best fit for clients.
-                    Besides software development, I am Prince2 and Professional Agile & Scrum certified with over 5 years of experience as a coordinator/manager within information systems and the hospitality industry.
-                    Here you can find more information on my projects.
+                <Col m={3}></Col>
+                <Col m={6} s={12} style = {{fontSize: '1.6rem', color: '#fff'}}>
+                    <p>As a software developer, my prior knowledge in project management makes me an asset for any product-minded team.
+                    I've spent the past 9 months learning web development and computer science at Lambda School, an innovative software engineering academy.</p>
+                    <p>I hope to use my education and experience to provide a unique perspective when designing software solutions for customers.
+                    I love to learn new frameworks and languages in order to keep my skill-set sharp and to ensure the applications I'm designing are the best fit for clients.</p>
+                    <p>Besides software development, I am Prince2 and Professional Agile & Scrum certified with over 5 years of experience as a coordinator/manager within information systems and the hospitality industry.</p>
+                    <p>Here you can find more information on my <Link to = '/projects'>projects</Link>.</p>
                 </Col>
+                <Col m={3}></Col>
             </Row>
         </Container>
     )
--- client/src/Components/Projects/Projects.js
+++ client/src/Components/Projects/Projects.js
@@ -1,13 +1,51 @@
-import React from 'react';
-import { Container } from 'react-materialize';
+import React, { Component } from 'react';
+import { Container, Row } from 'react-materialize';
+import Project from './Project';
+import gol from '../../Media/gameoflife.png';
 
-function Projects() {
-    return (
-        <Container>
-        <img className = 'bg' style = {{background: '#0B0B0B'}} alt = 'Projects' />
-            Projects
-        </Container>
-    )
+class Projects extends Component {
+    state = {
+        projects: [
+            {
+                'title': 'On The Record',
+                'link': '',
+                'description': '',
+                'image': ''
+
+            },
+            {
+                'title': 'MUD Game',
+                'link': '',
+                'description': '',
+                'image': ''
+            },
+            {
+                'title': 'Conways Life',
+                'link': 'https://dasmagameoflife.herokuapp.com/',
+                'description': '',
+                'image': gol
+            }
+        ]
+    }
+    render() {
+        return (
+            <Container>
+            <img className = 'bg' style = {{background: '#0B0B0B'}} alt = 'Projects' />
+                <Row style = {{color: '#fff'}}><h1>Projects</h1></Row>
+                {this.state.projects.map(project => {
+                    return (
+                        <Project 
+                            key = {project.id}
+                            title = {project.title}
+                            description = {project.description}
+                            image = {project.image}
+                            link = {project.link}
+                        />
+                    )
+                })}
+            </Container>
+        )
+    }
 }
 
 export default Projects;
