70fb67fe14b977372c5362621bdccea11df87eba
DasGMA <dasgrigoma@gmail.com>
2020-03-23 17:17:55 -0700

    Little changes

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Carousel } from 'react-materialize';
-import pdf from '../../Documents/dasma.pdf';
+import pdf from '../../CV/dasma.pdf';
 
 export default function Resume() {
    const options = {fullWidth: true, indicators: true};
